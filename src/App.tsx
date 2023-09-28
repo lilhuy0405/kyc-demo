aa73fef33d5ef1046a58031bc5a591acac3d14d6
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-09-28 18:05:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import {useState} from "react";
 
 declare global {
-  interface Window { Telegram: any; }
+  interface Window {
+    Telegram: any;
+  }
 }
 
 window.Telegram = window.Telegram || {};
 
 function App() {
+  const [telegramLoginData, setTelegramLoginData] = useState<any>(null);
   const handleTelegramLogin = () => {
     window.Telegram.Login.auth(
       {bot_id: '6546635625', request_access: true},
@@ -13,10 +17,12 @@ function App() {
         if (!data) {
           // authorization failed
           console.log("login failed");
+          return;
         }
 
         // Here you would want to validate data like described there https://core.telegram.org/widgets/login#checking-authorization
         console.log("login data", data)
+        setTelegramLoginData(data);
       }
     );
   }
@@ -26,6 +32,14 @@ function App() {
       <button onClick={handleTelegramLogin}>
         Login with Telegram
       </button>
+      <div>
+        {telegramLoginData && (
+          <>
+            <h2>Telegram Login Data</h2>
+            <pre>{JSON.stringify(telegramLoginData, null, 2)}</pre>
+          </>
+        )}
+      </div>
     </>
   )
 }
