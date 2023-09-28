51ea115cd9f03388bffefabfcadfe1b47df4aefb
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-09-28 16:05:49 +0700

    telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import {useEffect} from "react";
+
 function App() {
+  useEffect(() => {
+    //insert script tag
+    const script = document.createElement('script');
+    script.src = "https://sandbox.kyc-pass.com/kyc.js";
+  }, []);
   return (
     <>
       <h1>KYC Demo for Babbu</h1>
