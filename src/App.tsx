45ec256d1d3556222d612a87e6a09c390faeab0e
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-09-28 16:22:22 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import {useEffect} from "react";
 
 function App() {
-  useEffect(() => {
-    //insert script tag
-    const script = document.createElement('script');
-    script.src = "https://sandbox.kyc-pass.com/kyc.js";
-  }, []);
+  const handleTelegramLogin = () => {
+    //@ts-ignore
+    window.Telegram.Login.auth(
+      {bot_id: 'demo_babbu_login_bot', request_access: true},
+      (data: any) => {
+        if (!data) {
+          // authorization failed
+          console.log("login failed");
+        }
+
+        // Here you would want to validate data like described there https://core.telegram.org/widgets/login#checking-authorization
+        console.log("login data", data)
+      }
+    );
+  }
   return (
     <>
       <h1>KYC Demo for Babbu</h1>
+      <button onClick={handleTelegramLogin}>
+        Login with Telegram
+      </button>
     </>
   )
 }
